2309b5f4b3e2e42adb918e42f35fbbadd166c346
yogeshvaidya82 <yogesh2342@hotmail.com>
2014-12-18 23:08:44 -0800

    Implementing cache matrix implementation.
    Implementing cache matrix implementation and adding a message if value
    is retrieved from cache.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
+#returns cache matrix object list
+#list has three functions get,set, getinverse & setinverse
+#usage
+#source('cacheMatrix.R')
+#cacheAble <- makeCacheMatrix(m)
+#cacheSolve(cacheAble)
+#cacheSolve(cacheAble)
+makeCacheMatrix <- function(m = matrix()) {
+	 mInverse <- NULL
+	 set <- function(y) {
+			m <<- y
+            mInverse <<- NULL
+        }
+        get <- function() m
+        setinverse <- function(inverse) mInverse <<- inverse
+        getinverse <- function() mInverse
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+#cached solve object
+#first check if cached version exists else computes
+#matrix inverse and returns the value.
+cacheSolve <- function(m, ...) {
+	mInverse <- m$getinverse();
+	if (!is.null(mInverse)) {
+		message("returning cached version");
+		return(mInverse);
+	}
+	
+	matrixObj <- m$get();
+	mInverse <- solve(matrixObj);
+	m$setinverse(mInverse);
+	mInverse;
 }
